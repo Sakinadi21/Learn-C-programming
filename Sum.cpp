8c0f82a90aaf10598210c4102de2b8c040c305db
Sakiba <109652552+Sakinadi21@users.noreply.github.com>
2024-07-05 21:45:18 +0600

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int add( int a, int b){//(int a, int b=arguments
+return a+b;
+}
+
+int main (){
+ int a;
+ printf("Enter 1st number");
+ scanf("%d",&a);
+
+ int b;
+ printf("Enter 2nd number");
+ scanf("%d",&b);
+
+ int sum = add(a,b);
+ printf("%d",sum);
+ return 0;
+}
